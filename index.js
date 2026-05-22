ccff8f6b89e1f5f651ba56e76d930d029c96c2e1
peral1s <ruppera107@gmail.com>
2026-05-22 23:06:21 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot is running!');
+});
+
+app.listen(3000);
+
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
@@ -8,7 +17,20 @@ const client = new Client({
   ]
 });
 
-client.once('ready', () => {
+const replies = [
+  '…で？',
+  '…和紙が犯す',
+  '貴様誰zoy!?',
+  'そーいやRPG得意？',
+  '和紙のオリ？212cm128kgの漢と177cm70kgのおとなぁのおねぇさんよ',
+  '和紙はそこまで思ってなかったから意外だなぁ',
+  'あ、そーだ和紙のRPGやりなさい()',
+  '他者への幸福を邪魔する様な気持ちも捨てれる事こそが童帝の第一歩なのです',
+  '<@1286295306497032243>と言うわけで、貴方って生涯童帝を誓ってますかな？',
+  'だから童帝誓っても何のデメリットも無いに決まってんだろ常識的に考えて'
+];
+
+client.once('clientReady', () => {
   console.log('Bot起動！');
 });
 
@@ -17,9 +39,14 @@ client.on('messageCreate', message => {
 
   const random = Math.random();
 
-  // 20%の確率で送信
+  // 20%で返信
   if (random < 0.05) {
-    message.channel.send('…で？');
+
+    // ランダムなセリフ選択
+    const reply =
+      replies[Math.floor(Math.random() * replies.length)];
+
+    message.channel.send(reply);
   }
 });
 
