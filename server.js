a2cd88b57214c96db5ced0aa4dd2d3ccbd0972ad
peral1s <ruppera107@gmail.com>
2026-07-23 04:29:32 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const express = require('express');
+const app = express();
 
-function startServer() {
-  const app = express();
+// Render が自動で割り当てる PORT を優先的に使用します
+const PORT = process.env.PORT || 3000;
 
-  app.get('/', (req, res) => {
-    res.send('Bot is running!');
-  });
+app.get('/', (req, res) => {
+  res.send('Bot is running!');
+});
 
-  app.listen(process.env.PORT || 3000, () => {
-    console.log('Web server started');
-  });
-}
-
-module.exports = startServer;
-
+app.listen(PORT, () => {
+  console.log(`🌐 サーバーがポート ${PORT} で起動したのだ`);
+});
