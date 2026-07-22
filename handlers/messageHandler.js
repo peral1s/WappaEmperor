90e9c3eb3290d49f67e3580445577dbf5aa5dd93
peral1s <ruppera107@gmail.com>
2026-07-23 04:13:48 +0900

    Update messageHandler.js

--- handlers/messageHandler.js
+++ handlers/messageHandler.js
@@ -1,5 +1,6 @@
 const replyData = require('../data/replies');
 const { updateUserStats } = require('../db/database');
+const { handleMusicCommands } = require('../managers/musicManager');
 
 async function handleMessage(message, client) {
   if (message.author.bot) return;
@@ -19,6 +20,12 @@ async function handleMessage(message, client) {
         console.error('ロール付与失敗:', err);
       }
     }
+  }
+
+  // 🎵 音楽コマンド判定 (w! から始まるメッセージ)
+  if (message.content.startsWith('w!')) {
+    await handleMusicCommands(message);
+    return;
   }
 
   const content = message.content.toLowerCase();
@@ -67,7 +74,7 @@ async function handleMessage(message, client) {
   }
 
   if (message.content.includes('彼女')) {
-    message.channel.send('彼女を検出！！！！！！\n嫉嫉妬モード発動！！！！！\nンニィィィィィィィィィィィィィ');
+    message.channel.send('彼女を検出！！！！！！\n嫉嫉嫉モード発動！！！！！\nンニィィィィィィィィィィィィィ');
   }
 
   const sexWords = ['セックス', 'サックス', 'シックス', 'ソックス', 'せっくす', 'sax', 'six', 'sex', 'sox', 'せくす', 'セクス', 'せくしー', 'セクシー'];
@@ -107,4 +114,3 @@ async function handleMessage(message, client) {
 }
 
 module.exports = handleMessage;
-
